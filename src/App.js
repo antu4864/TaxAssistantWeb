682a94f3cc7cbfadd0a224560f6331a4beece4d5
Antu Kumar <67988092+antu4864@users.noreply.github.com>
2021-05-27 18:18:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <Route path='/It' component={It}/>
         <Route path='/Pas' component={Pas}/>
         <Route path='/Vat' component={Vat}/>
-       
+        <Route component={Error} />
       </Switch>
  
    </>
